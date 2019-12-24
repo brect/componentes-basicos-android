2f1ba487d2091c68f307c0e7cca344c61ac3bcf6
brect <b.limas93@gmail.com>
2019-12-24 01:05:50 -0300

    Adicionado radio buttom no projeto;

--- app/src/main/java/br/com/blimas/componentesBasicos/MainActivity.java
+++ app/src/main/java/br/com/blimas/componentesBasicos/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultado;
     private CheckBox checkBoxBranco, checkBoxVerde, checkBoxVermelho;
     List<String> check = new ArrayList<String>();
+    private RadioGroup radioGroupEstoque;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,30 +30,51 @@ public class MainActivity extends AppCompatActivity {
         checkBoxBranco = findViewById(R.id.checkBox_cor_branco);
         checkBoxVerde = findViewById(R.id.checkBox_cor_verde);
         checkBoxVermelho = findViewById(R.id.checkBox_cor_vermelho);
+
+        verificaRadioButton();
     }
 
-    public void btnEnviar(View view){
-        String valorTextoProduto = campoProduto.getText().toString();
-        resultado.setText(valorTextoProduto);
+    public void btnEnviar(View view) {
+
+        //Recupera valores do EditText
+//        String valorTextoProduto = campoProduto.getText().toString();
+//        resultado.setText(valorTextoProduto);
+
+        verificaCheckBox();
+
+
     }
 
-    public void verificaCheckBox (){
+    public void verificaCheckBox() {
         //verifica e limpa a lista antes de salvar o valor
         check.clear();
 
 
         //verifica se o checkbox esta ativo
-        if (checkBoxBranco.isChecked()){
+        if (checkBoxBranco.isChecked()) {
             check.add(checkBoxBranco.getText().toString());
         }
-        if (checkBoxVerde.isChecked()){
+        if (checkBoxVerde.isChecked()) {
             check.add(checkBoxVerde.getText().toString());
         }
-        if (checkBoxVermelho.isChecked()){
+        if (checkBoxVermelho.isChecked()) {
             check.add(checkBoxVermelho.getText().toString());
         }
 
         //retorna valores selecionados pelo usuario
         resultado.setText(check.toString());
+    }
+
+    public void verificaRadioButton() {
+        radioGroupEstoque.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButton_sim) {
+                    resultado.setText("Sim");
+                } else{
+                    resultado.setText("Não");
+                }
+            }
+        });
     }
 }
